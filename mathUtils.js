5aa7c20c0716bfc74ef1a4430d80f9189d83157b
Bettina-Sam <159879671+Bettina-Sam@users.noreply.github.com>
2025-08-17 15:21:29 +0530

    Delete mathUtils.js

--- mathUtils.js
+++
@@ -1,6 +0,0 @@
-export function add(a, b) { return Number(a) + Number(b); }
-export function multiply(a, b) { return Number(a) * Number(b); }
-
-export default function greet(name) {
-  return `Hello, ${name}! (from default export)`;
-}
